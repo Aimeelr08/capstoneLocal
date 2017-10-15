f4a08044508c3f6dd15ac0532cb7172f22c62965
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-14 21:08:37 -0400

    host changed to backendcapstonelocal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,4 @@
 'use strict';
-// const http = require("http");
-// setInterval(function() {
-//     http.get("https://backendcapstonelocal.herokuapp.com/");
-// }, 300000);
 
 const app = {
   // host: 'http://localhost:4741/'
@@ -11,5 +7,4 @@ host: 'https://backendcapstonelocal.herokuapp.com/'
 
 module.exports = {
   app
-  // http
 }
